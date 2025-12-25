1565f72f872d3fcee4dde8f4b8bf15d7ada50131
web3chong <web3chong@gmail.com>
2025-12-25 18:00:17 +0800

    feat: 新增 /info 接口，返回请求来源 IP 和设备信息

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -92,6 +93,42 @@ func main() {
 		})
 	})
 
+	// 获取请求来源 IP 和设备信息
+	router.GET("/info", func(c *gin.Context) {
+		// 获取客户端 IP
+		clientIP := c.ClientIP()
+		
+		// 获取真实 IP（考虑代理）
+		realIP := c.GetHeader("X-Real-IP")
+		if realIP == "" {
+			realIP = c.GetHeader("X-Forwarded-For")
+			if realIP != "" {
+				// X-Forwarded-For 可能包含多个 IP，取第一个
+				ips := strings.Split(realIP, ",")
+				if len(ips) > 0 {
+					realIP = strings.TrimSpace(ips[0])
+				}
+			}
+		}
+		if realIP == "" {
+			realIP = clientIP
+		}
+
+		// 获取设备信息
+		userAgent := c.GetHeader("User-Agent")
+		
+		// 解析 User-Agent 获取设备信息
+		deviceInfo := parseUserAgent(userAgent)
+
+		c.JSON(http.StatusOK, gin.H{
+			"ip":           realIP,
+			"client_ip":    clientIP,
+			"user_agent":   userAgent,
+			"device_info":  deviceInfo,
+			"request_time": time.Now().Format("2006-01-02 15:04:05"),
+		})
+	})
+
 	// 启动 HTTP 服务器，明确绑定到 0.0.0.0 以支持公网访问
 	addr := serverHost + ":" + serverPort
 	srv := &http.Server{
@@ -121,4 +158,91 @@ func main() {
 
 	log.Println("正在关闭服务器...")
 	os.Exit(0)
+}
+
+// parseUserAgent 解析 User-Agent 获取设备信息
+func parseUserAgent(userAgent string) map[string]string {
+	info := make(map[string]string)
+	ua := strings.ToLower(userAgent)
+
+	// 操作系统检测
+	if strings.Contains(ua, "windows") {
+		info["os"] = "Windows"
+		if strings.Contains(ua, "windows nt 10.0") || strings.Contains(ua, "windows 10") {
+			info["os_version"] = "Windows 10/11"
+		} else if strings.Contains(ua, "windows nt 6.3") {
+			info["os_version"] = "Windows 8.1"
+		} else if strings.Contains(ua, "windows nt 6.2") {
+			info["os_version"] = "Windows 8"
+		} else if strings.Contains(ua, "windows nt 6.1") {
+			info["os_version"] = "Windows 7"
+		} else {
+			info["os_version"] = "Windows"
+		}
+	} else if strings.Contains(ua, "mac os x") || strings.Contains(ua, "macintosh") {
+		info["os"] = "macOS"
+		if strings.Contains(ua, "mac os x 10_") {
+			// 提取版本号
+			parts := strings.Split(ua, "mac os x ")
+			if len(parts) > 1 {
+				version := strings.Split(parts[1], "_")[0]
+				info["os_version"] = "macOS " + version
+			}
+		}
+	} else if strings.Contains(ua, "linux") {
+		info["os"] = "Linux"
+	} else if strings.Contains(ua, "android") {
+		info["os"] = "Android"
+		// 提取 Android 版本
+		if strings.Contains(ua, "android ") {
+			parts := strings.Split(ua, "android ")
+			if len(parts) > 1 {
+				version := strings.Fields(parts[1])[0]
+				info["os_version"] = "Android " + version
+			}
+		}
+	} else if strings.Contains(ua, "iphone") || strings.Contains(ua, "ipad") {
+		info["os"] = "iOS"
+		if strings.Contains(ua, "os ") {
+			parts := strings.Split(ua, "os ")
+			if len(parts) > 1 {
+				version := strings.Split(strings.Fields(parts[1])[0], "_")
+				if len(version) >= 2 {
+					info["os_version"] = "iOS " + version[0] + "." + version[1]
+				}
+			}
+		}
+	} else {
+		info["os"] = "Unknown"
+	}
+
+	// 浏览器/客户端检测
+	if strings.Contains(ua, "chrome") && !strings.Contains(ua, "edg") {
+		info["browser"] = "Chrome"
+	} else if strings.Contains(ua, "firefox") {
+		info["browser"] = "Firefox"
+	} else if strings.Contains(ua, "safari") && !strings.Contains(ua, "chrome") {
+		info["browser"] = "Safari"
+	} else if strings.Contains(ua, "edg") {
+		info["browser"] = "Edge"
+	} else if strings.Contains(ua, "opera") {
+		info["browser"] = "Opera"
+	} else if strings.Contains(ua, "curl") {
+		info["browser"] = "curl"
+	} else if strings.Contains(ua, "postman") {
+		info["browser"] = "Postman"
+	} else {
+		info["browser"] = "Unknown"
+	}
+
+	// 设备类型
+	if strings.Contains(ua, "mobile") || strings.Contains(ua, "android") || strings.Contains(ua, "iphone") {
+		info["device_type"] = "Mobile"
+	} else if strings.Contains(ua, "tablet") || strings.Contains(ua, "ipad") {
+		info["device_type"] = "Tablet"
+	} else {
+		info["device_type"] = "Desktop"
+	}
+
+	return info
 }
